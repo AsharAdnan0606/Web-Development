a01ad0ccd8d6222e2e1b1bf2db0c53df9d57982f
AsharAdnan0606 <oashar866@gmail.com>
2026-07-11 10:16:14 +0500

    feat: add game buttons and layout styling

--- JS/tic-tac-toe/tic-tac-toe.js
+++ JS/tic-tac-toe/tic-tac-toe.js
@@ -71,22 +71,24 @@ start.addEventListener('click' ,()=>{
 
 function displayGrid(){
     document.body.innerHTML=`
-    <h1>
-      Tic Tac Toe
-    </h1>
-    <div class="tic-tac-toe-grid">
-      <div class="general-cell cell1" onclick="makemove(0)">${grid[0]}</div>
-      <div class="general-cell cell2" onclick="makemove(1)">${grid[1]}</div>
-      <div class="general-cell cell3" onclick="makemove(2)">${grid[2]}</div>
-      <div class="general-cell cell4" onclick="makemove(3)">${grid[3]}</div>
-      <div class="general-cell cell5" onclick="makemove(4)">${grid[4]}</div>
-      <div class="general-cell cell6" onclick="makemove(5)">${grid[5]}</div>
-      <div class="general-cell cell7" onclick="makemove(6)">${grid[6]}</div>
-      <div class="general-cell cell8" onclick="makemove(7)">${grid[7]}</div>
-      <div class="general-cell cell9" onclick="makemove(8)">${grid[8]}</div>
-      <div class="winner">winner: ${winner} </div>            
+    <div class="game-layout">
+      <h1>
+        Tic Tac Toe
+      </h1>
+      <div class="tic-tac-toe-grid">
+        <div class="general-cell cell1" onclick="makemove(0)">${grid[0]}</div>
+        <div class="general-cell cell2" onclick="makemove(1)">${grid[1]}</div>
+        <div class="general-cell cell3" onclick="makemove(2)">${grid[2]}</div>
+        <div class="general-cell cell4" onclick="makemove(3)">${grid[3]}</div>
+        <div class="general-cell cell5" onclick="makemove(4)">${grid[4]}</div>
+        <div class="general-cell cell6" onclick="makemove(5)">${grid[5]}</div>
+        <div class="general-cell cell7" onclick="makemove(6)">${grid[6]}</div>
+        <div class="general-cell cell8" onclick="makemove(7)">${grid[7]}</div>
+        <div class="general-cell cell9" onclick="makemove(8)">${grid[8]}</div>
+        <div class="winner">winner: ${winner} </div>            
+      </div>
+      <button class="general-button" onclick="reset()">RESET</button>
     </div>
-    <button onclick="reset()">reset</button>
   `;
   
 }
